04bb6d3f13b9c67dc3b02443eab9b0eda126fcb4
ArpithaNagappa <arpithagn021@gmail.com>
2021-03-01 18:35:23 +0000

    Update 30

--- scrabble-scorer.js
+++ scrabble-scorer.js
@@ -114,9 +114,9 @@ function scrabbleScore(word) {
  }
 
 const scoringAlgorithms = [
-['name: Simple Score', 'description: Each letter is worth 1 point.','scoringFunction: simpleScore'],
-['name: Bonus Vowels','description: Vowels are 3 pts, consonants are 1 pt.','scoringFunction: vowelBonusScore'],
-['name: Scrabble','description: The traditional scoring algorithm.','scoringFunction: scrabbleScore']
+{'name: Simple Score', 'description: Each letter is worth 1 point.','scoringFunction: simpleScore'},
+{'name: Bonus Vowels','description: Vowels are 3 pts, consonants are 1 pt.','scoringFunction: vowelBonusScore'},
+{'name: Scrabble','description: The traditional scoring algorithm.','scoringFunction: scrabbleScore'}
 ];
 
 function scorerPrompt(word) {
