61943c0752b6bbd3e841f4d7eb8b160ea43414d3
ArpithaNagappa <arpithagn021@gmail.com>
2021-03-01 00:51:43 +0000

    First Commit

--- scrabble-scorer.js
+++ scrabble-scorer.js
@@ -12,16 +12,19 @@ const oldPointStructure = {
   10: ['Q', 'Z']
 };
 
+
 function oldScrabbleScorer(word) {
 	word = word.toUpperCase();
-	let letterPoints = "";
+	//let letterPoints = "";
+  let letterPoints=0;
  
 	for (let i = 0; i < word.length; i++) {
  
 	  for (const pointValue in oldPointStructure) {
  
 		 if (oldPointStructure[pointValue].includes(word[i])) {
-			letterPoints += `Points for '${word[i]}': ${pointValue}\n`
+			//letterPoints += `Points for '${word[i]}': ${pointValue}\n`
+      letterPoints += transform(word[i]);
 		 }
  
 	  }
@@ -33,26 +36,145 @@ function oldScrabbleScorer(word) {
 // don't change the names or your program won't work as expected. //
 
 function initialPrompt() {
-   console.log("Let's play some scrabble! Enter a word:");
+   let response = input.question("Let's play some scrabble! Enter a word : ");
+   //console.log(oldScrabbleScorer(response));
+   return response;
 };
 
 let simpleScore;
 
+const simplePointStructure = {
+  1: ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+};
+
+function simpleScorer(word) {
+	word = word.toUpperCase();
+	//let simpleScore = "";
+  let simpleScore=0;
+ 
+	for (let i = 0; i < word.length; i++) {
+ 
+	  for (const pointValue in simplePointStructure) {
+ 
+		 if (simplePointStructure[pointValue].includes(word[i])) {
+			//simpleScore += `Points for '${word[i]}': ${pointValue}\n`
+      simpleScore += Number(pointValue);
+		 }
+ 
+	  }
+	}
+	return simpleScore;
+ }
+
 let vowelBonusScore;
+
+const vowelPointStructure = {
+  1: ['B', 'C', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'V', 'W', 'X', 'Y', 'Z'],
+  3: ['A', 'E', 'I', 'O', 'U']
+};
+
+function vowelBonusScorer(word) {
+	word = word.toUpperCase();
+	//let vowelBonusScore = "";
+  let vowelBonusScore=0;
+ 
+	for (let i = 0; i < word.length; i++) {
+ 
+	  for (const pointValue in vowelPointStructure) {
+ 
+		 if (vowelPointStructure[pointValue].includes(word[i])) {
+			//vowelBonusScore += `Points for '${word[i]}': ${pointValue}\n`
+      vowelBonusScore += Number(pointValue);
+		 }
+ 
+	  }
+	}
+	return vowelBonusScore;
+ }
 
 let scrabbleScore;
 
-const scoringAlgorithms = [];
+const scoringAlgorithms = [
+  ['Simple Score','Each letter is worth 1 point.','oldScrabbleScorer'],['Bonus Vowels','	Vowels are 3 pts, consonants are 1 pt.','simpleScore'],['Scrabble','The traditional scoring algorithm.','vowelBonusScore']
+];
 
-function scorerPrompt() {}
+function scorerPrompt(word) {
+  let score;
+  console.log("Which scoring algorithm would you like to use?\n");
 
-function transform() {};
+  console.log("0 - Simple: One point per character");
+  console.log("1 - Vowel Bonus: Vowels are worth 3 points");
+  console.log("2 - Scrabble: Uses scrabble point system");
+  let response = input.question("Enter 0, 1, or 2: ");
+  if (response == 0)
+  {
+    score = simpleScorer(word);
+  }
+  else if (response == 1)
+  {
+    score = vowelBonusScorer(word);
+  }
+  else if (response == 2)
+  {
+    score = oldScrabbleScorer(word);
+  }
+  console.log("Score for 'coconut': "+score);
+}
 
-let newPointStructure;
+
+const newPointStructure = {
+  'a': 1,
+  'b': 3,
+  'c': 3,
+  'd': 2,
+  'e': 1,
+  'f': 4,
+  'g': 2,
+  'h': 4,
+  'i': 1,
+  'j': 8,
+  'k': 5,
+  'l': 1,
+  'm': 3,
+  'n': 1,
+  'o': 1,
+  'p': 3,
+  'q': 10,
+  'r': 1,
+  's': 1,
+  't': 1,
+  'u': 1,
+  'v': 4,
+  'w': 4,
+  'x': 8,
+  'y': 4,
+  'z': 10
+};
+
+//let newPointStructure;
+
+function transform(key) 
+{
+  key = key.toLowerCase();
+	//let letterPoints = "";
+  let letterPoints=0;	
+ 
+	  for (const pointValue in newPointStructure) {
+ 
+	   if (key == pointValue)
+     {
+       letterPoints += newPointStructure[key];
+     }
+ 
+	  }
+	
+	return letterPoints;
+};
+
 
 function runProgram() {
-   initialPrompt();
-   
+   let word = initialPrompt();
+   scorerPrompt(word);
 }
 
 // Don't write any code below this line //
