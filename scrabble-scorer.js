24144e3edac04b6e7f6293f2a6a59ecb2a5ac4aa
ArpithaNagappa <arpithagn021@gmail.com>
2021-03-01 05:50:48 +0000

    Update 15

--- scrabble-scorer.js
+++ scrabble-scorer.js
@@ -113,7 +113,7 @@ function scrabbleScore(word) {
 	return letterPoints;
  }
 
-const scoringAlgorithms = [ [ 'Simple Score', 'Each letter is worth 1 point.', 'scoringFunction: simpleScore()' ], [ 'Bonus Vowels', 'Vowels are 3 pts, consonants are 1 pt.', 'scoringFunction: vowelBonusScore()' ], [ 'Scrabble', 'The traditional scoring algorithm.', 'scoringFunction: oldScrabbleScorer()' ] ];
+const scoringAlgorithms = [ [ 'Simple Score', 'Each letter is worth 1 point.', `simpleScore()` ], [ 'Bonus Vowels', 'Vowels are 3 pts, consonants are 1 pt.', `vowelBonusScore()` ], [ 'Scrabble', 'The traditional scoring algorithm.', `oldScrabbleScorer()` ] ];
 
 function scorerPrompt(word) {
   let score;
